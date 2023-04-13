c9938786e846e11c4fed27b52adcdf1929515902
Bruno Görresen Mello <bruno@aetos.tech>
2023-04-13 11:49:03 -0300

    Improving code and adding profiler

--- tsd.py
+++ tsd.py
@@ -3,48 +3,60 @@ import random
 from scipy.optimize import minimize_scalar
 import time
 from functools import partial
+from utils import h
+from profiler import profile_each_line
 
-def h(f, Y):
-    return np.sum((Y.flatten() - f)**2)
+def h_B_plus_theta(coeffs, theta):
+    """
+    We don't sum h_B because it is constant and thus doesn't affect the minimum
+    """
+    return (
+        coeffs[0] * theta**4
+        + coeffs[1] * theta**3
+        + coeffs[2] * theta**2
+        + coeffs[3] * theta
+    )
 
-def f(B_t_v):
-    return np.sum(B_t_v*B_t_v, axis=0)
 
-def h_B_plus_theta(B_t_X_j, Y_minus_f, X_i, theta, h_B):
-    w = 2 * theta * X_i * B_t_X_j + theta**2 * X_i**2
-    return np.sum(h_B - 2*(Y_minus_f)*w + w**2)
-
+@profile_each_line
 def tsd_trace_regression(X, Y, max_iters=100, verbose=True):
     N, n = X.shape
     B = np.identity(n)
     coord = [(i, j) for i in range(n) for j in range(i, n)]
-    objectives = [h(f(B.T@X.T), Y)]
+    objectives = [h(X, Y, B)]
     times = []
-    minimize_times = []
+
+    B_t_X = B.T @ X.T
+    X_columns = [X[:, i] for i in range(n)]
+    X_columns_squared = [X_columns[i]**2 for i in range(n)]
+    X_columns_cubed = [X_columns_squared[i] * X_columns[i] for i in range(n)]
+    X_columns_quadrupled = [X_columns_squared[i]**2 for i in range(n)]
+    X_columns_quadrupled_sum = [np.sum(X_columns_quadrupled[i]) for i in range(n)]
 
     for t in range(max_iters):
         start = time.time()
         random.shuffle(coord)
 
-        for (i, j) in coord:
-            B_t_X = B.T @ X.T
-            f_B_X = f(B_t_X)
-            h_B = h(f_B_X, Y)
-            X_i = X[:, i]
+
+        for i, j in coord:
+
+            f_B_X = np.sum(B_t_X * B_t_X, axis=0)
             Y_minus_f = Y - f_B_X
-            B_t_X_j = B_t_X[j]
-
-            min_funcion = partial(h_B_plus_theta, B_t_X_j, Y_minus_f, X_i, h_B=h_B)
-            
-            minimize_time = time.time()
+            coeffs = [
+                X_columns_quadrupled_sum[i],
+                np.sum(4 * X_columns_cubed[i] * B_t_X[j]),
+                np.sum(2*(2*B_t_X[j]**2 - 2 * Y_minus_f) * X_columns_squared[i]),
+                -np.sum(4 * Y_minus_f * X_columns[i] * B_t_X[j]),
+            ]
+            min_funcion = partial(h_B_plus_theta, coeffs)
             theta = minimize_scalar(min_funcion).x
-            minimize_times.append(time.time() - minimize_time)
 
-            B += theta * np.outer(np.eye(n)[i], np.eye(n)[j])
+            B[i, j] += theta
+            B_t_X[j] += theta * X_columns[i].T
 
         loop_time = time.time() - start
 
-        h_B = h(f(B.T@X.T), Y)
+        h_B = h(X, Y, B)
 
         if verbose:
             print(f"Iteration: {t+1}, objective: {h_B}, time: {loop_time}")
@@ -52,4 +64,4 @@ def tsd_trace_regression(X, Y, max_iters=100, verbose=True):
         objectives.append(h_B)
         times.append(loop_time)
 
-    return B, objectives, times, minimize_times
+    return B, objectives, times
