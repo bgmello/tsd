a2c936dea1498a8c2c4a3eb9f4ad1630e2965481
Bruno Görresen Mello <bruno@aetos.tech>
2023-05-03 12:41:17 -0300

    running in parallel

--- wishart_test.py
+++ wishart_test.py
@@ -1,10 +1,14 @@
 from tsd import TSDTraceRegression
 import json
 from rgd import rgd_trace_regression
+import sys
 
+from multiprocessing import Pool
 import numpy as np
 
 np.random.seed(42)
+
+tot_time = int(sys.argv[1])
 
 
 def generate_wishart(m, d, r):
@@ -23,26 +27,34 @@ def generate_wishart(m, d, r):
     return X, Y
 
 
+def generate_data(params):
+    X, Y = generate_wishart(*params)
+    _, objectives_t, times_t, inner_times = TSDTraceRegression().fit(X, Y, tot_time, verbose=True)
+    _, objectives_t_rand, times_t_rand, inner_times_rand = TSDTraceRegression().fit(X, Y, tot_time, verbose=True, randomized=True)
+    _, objectives_r, times_r = rgd_trace_regression(X, Y, tot_time)
+    with open(f"wishart_{'_'.join([str(param) for param in params])}.json", "w") as f:
+        f.write(json.dumps({"max_time": tot_time,
+                            "tsd_objective": objectives_t,
+                            "tsd_objective_rand": objectives_t_rand,
+                            "rgd_objective": objectives_r,
+                            "tsd_time": times_t,
+                            "tsd_time_rand": times_t_rand,
+                            "tsd_inner_time": inner_times,
+                            "tsd_inner_time_rand": inner_times_rand,
+                            "rgd_time": times_r}))
+
+
 if __name__ == "__main__":
-    tot_time = 20
 
     test_data = [
         (1000, 50, 50),
-        (1000, 50, 10)
+        (1000, 50, 10),
+        (10000, 50, 50),
+        (10000, 50, 100),
+        (100000, 50, 50),
+        (100000, 500, 500),
+        (100000, 500, 100),
     ]
 
-    for params in test_data:
-        X, Y = generate_wishart(*params)
-        _, objectives_t, times_t, inner_times = TSDTraceRegression().fit(X, Y, tot_time, verbose=True)
-        _, objectives_t_rand, times_t_rand, inner_times_rand = TSDTraceRegression().fit(X, Y, tot_time, verbose=True, randomized=True)
-        _, objectives_r, times_r = rgd_trace_regression(X, Y, tot_time)
-        with open(f"wishart_{'_'.join([str(param) for param in params])}.json", "w") as f:
-            f.write(json.dumps({"max_time": tot_time,
-                                "tsd_objective": objectives_t,
-                                "tsd_objective_rand": objectives_t_rand,
-                                "rgd_objective": objectives_r,
-                                "tsd_time": times_t,
-                                "tsd_time_rand": times_t_rand,
-                                "tsd_inner_time": inner_times,
-                                "tsd_inner_time_rand": inner_times_rand,
-                                "rgd_time": times_r}))
+    pool = Pool(processes=len(test_data))
+    pool.map(generate_data, test_data)
