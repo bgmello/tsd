18bf6b7b52efc385c9472caec4515df2bf217a1e
Bruno Mello <bgorresenm@gmail.com>
2023-07-06 17:42:19 +0000

    improving generation of final data

--- generate_final_data.py
+++ generate_final_data.py
@@ -8,10 +8,15 @@ from typing import List, Tuple
 def get_consistent_ys(line) -> np.array:
     line = np.array(line)
     x_ticks = np.linspace(0, 100, 201)
-    line_arr = np.array(line)
-    idx = np.searchsorted(line_arr[:, 0], x_ticks, side='right') - 1
-    idx[idx < 0] = 0
-    return line_arr[idx, 1]
+    max_y_values = []
+
+    for x_tick in x_ticks:
+        # Find the y values for x <= x_tick
+        y_values = line[line[:, 0] <= x_tick, 1]
+        # If there are any y values, find the max, otherwise use NaN (or some other value)
+        max_y = np.max(y_values) if len(y_values) > 0 else np.nan
+        max_y_values.append(max_y)
+    return np.array(max_y_values)
 
 
 def merge_lines(lines: List[np.ndarray]) -> Tuple[np.array, np.array]:
@@ -20,30 +25,32 @@ def merge_lines(lines: List[np.ndarray]) -> Tuple[np.array, np.array]:
     y_avgs = np.mean(consistent_ys, axis=0)
     return x_ticks, y_avgs
 
-def get_xs_ys(iteration: dict) -> Tuple[np.array, np.array, np.array]:
 
-    def process_algorithm(times, objectives):
-        times_cum = np.array(times).cumsum()
-        times_norm = (100 * times_cum / max_time)
-        objectives_norm = np.array(objectives)
-        mask = times_norm <= 100
-        return np.vstack((times_norm[mask], objectives_norm[mask])).T
+def get_xs_ys(iteration: dict) -> Tuple[List[Tuple[float, float]], List[Tuple[float, float]], List[Tuple[float, float]]]:
+    rgd_times = np.array(iteration["rgd_time"]).cumsum()
+    max_time = rgd_times[rgd_times < iteration["max_time"]][-1]
 
-    rgd_times_cum = np.array(iteration["rgd_time"]).cumsum()
-    max_time = rgd_times_cum[rgd_times_cum < iteration["max_time"]][-1]
+    tsd_times = np.array(iteration['tsd_inner_time']).cumsum()/max_time
+    tsd_rand_times = np.array(iteration['tsd_inner_time_rand']).cumsum()/max_time
+    rgd_times = np.array(iteration['rgd_time']).cumsum()/max_time
 
-    tsd = process_algorithm(iteration['tsd_inner_time'], iteration['tsd_inner_objective'])
-    tsd_rand = process_algorithm(iteration['tsd_inner_time_rand'], iteration['tsd_inner_objective_rand'])
-    rgd = process_algorithm(iteration['rgd_time'], iteration['rgd_objective'])
+    tsd_objective_filtered = [obj for time, obj in zip(tsd_times, iteration['tsd_inner_objective']) if time <= 1]
+    tsd_objective_rand_filtered = [obj for time, obj in zip(tsd_rand_times, iteration['tsd_inner_objective_rand']) if time <= 1]
+    rgd_objective_filtered = [obj for time, obj in zip(rgd_times, iteration['rgd_objective']) if time <= 1]
 
-    # Get the final error based on the final objectives of each algorithm
-    final_error = min(tsd[1, -1], tsd_rand[1, -1], rgd[1, -1])
+    final_error = min(min(tsd_objective_filtered, default=float('inf')),
+                      min(tsd_objective_rand_filtered, default=float('inf')),
+                      min(rgd_objective_filtered, default=float('inf')))
 
-    tsd[1, :] = 100 * final_error / tsd[1, :]
-    tsd_rand[1, :] = 100 * final_error / tsd_rand[1, :]
-    rgd[1, :] = 100 * final_error / rgd[1, :]
+    tsd = [(100*time, 100*final_error/obj)
+           for time, obj in zip(tsd_times, iteration['tsd_inner_objective']) if time <= 1]
+    tsd_rand = [(100*time, 100*final_error/obj)
+                for time, obj in zip(tsd_rand_times, iteration['tsd_inner_objective_rand']) if time <= 1]
+    rgd = [(100*time, 100*final_error/obj)
+           for time, obj in zip(rgd_times, iteration['rgd_objective']) if time <= 1]
 
     return tsd, tsd_rand, rgd
+
 
 def get_single_lines(data: List[dict]) -> Tuple[np.array, np.array, np.array]:
     tsds, tsds_rand, rgds = [], [], []
@@ -77,36 +84,8 @@ def generate_single_lines(m, d, r, data: List[dict]) -> plt.Figure:
     }
 
 
-def generate_multi_lines(m, d, r, data: List[dict]) -> plt.Figure:
-    fig, axes = plt.subplots(figsize=[10, 6], ncols=1, nrows=1, constrained_layout=True)
-
-    final_data = []
-
-    for iteration in data:
-
-
-        tsd_line, tsd_rand_line, rgd_line = get_xs_ys(iteration)
-
-        y_tsd = get_consistent_ys(tsd_line)
-        y_tsd_rand = get_consistent_ys(tsd_rand_line)
-        y_rgd = get_consistent_ys(rgd_line)
-        x_ticks = np.linspace(0, 100, 201)
-
-        final_data.append({
-            "x_tsd": list(x_ticks),
-            "y_tsd": list(y_tsd),
-            "x_tsd_rand": list(x_ticks),
-            "y_tsd_rand": list(y_tsd_rand),
-            "x_rgd": list(x_ticks),
-            "y_rgd": list(y_rgd)
-            })
-
-
-    return final_data
-
-
 def plot_instance(m, d, r):
-    if os.path.exists(f"data/final_data_{m}_{d}_{r}_multi.json") and os.path.exists(f"data/final_data_{m}_{d}_{r}_single.json"):
+    if os.path.exists(f"data/final_data_{m}_{d}_{r}_single.json"):
         return
     data = []
 
@@ -116,12 +95,8 @@ def plot_instance(m, d, r):
             with open(f"data/wishart_{m}_{d}_{r}_seed_{seed}_algo_{algo}.json", "r") as f:
                 tmp = tmp | json.loads(f.read())
         data.append(tmp)
-    if not os.path.exists(f"data/final_data_{m}_{d}_{r}_single.json"):
-        with open(f"data/final_data_{m}_{d}_{r}_single.json", "w") as f:
-            f.write(json.dumps(generate_single_lines(m, d, r, data)))
-    if not os.path.exists(f"data/final_data_{m}_{d}_{r}_multi.json"):
-        with open(f"data/final_data_{m}_{d}_{r}_multi.json", "w") as f:
-            f.write(json.dumps(generate_multi_lines(m, d, r, data)))
+    with open(f"data/final_data_{m}_{d}_{r}_single.json", "w") as f:
+        f.write(json.dumps(generate_single_lines(m, d, r, data)))
 
 
 if __name__ == "__main__":
